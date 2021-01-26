b4381452c8c93c52428d8a594d4ba51358dcdce7
Kyle Sherrington <kylejsherrington@gmail.com>
2021-01-26 09:29:00 -0800

    Initial commit

--- card-challenge-2/ContentView.swift
+++ card-challenge-2/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+struct Card {
+    var card = 1
+    var suit = 0
+}
+
 struct ContentView: View {
+    
+    @State var deck = [Card]()
+    @State var message = ""
+    @State var generatedLog = [String]()
+    var suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack (spacing: 10.0) {
+            Text(message)
+            
+            HStack (spacing: 10.0) {
+                Button("Add Card") {
+                     addCard()
+                }
+                Button("Draw Card") {
+                     drawCard()
+                }
+            }
+        }
+    }
+    
+    func addCard() {
+        let randNum = Int.random(in: 1...13)
+        let randSuit = Int.random(in: 0...3)
+        
+        var newCard = Card()
+        newCard.card = randNum
+        newCard.suit = randSuit
+    }
+    
+    func drawCard() {
+        
     }
 }
 
